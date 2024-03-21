1def82726833a4deca1731d15ebfc042a1fb6f86
lele-rdm <leandromartinspinto2004@gmail.com>
2024-03-21 12:22:01 -0300

    Adicionando construtores da classe Conta

--- Sistema_Bancario/Conta.cs
+++ Sistema_Bancario/Conta.cs
@@ -8,5 +8,22 @@ namespace Sistema_Bancario
 {
     internal class Conta
     {
+        public int Numero { get; set; }
+        public string Titular { get; set; }
+        public double Saldo { get; }
+
+        public Conta(int numero, string titular)
+        {
+            Numero = numero;
+            Titular = titular;
+            Saldo = 0;
+        }
+
+        public Conta(int numero, string titular, double depositoInicial)
+        {
+            Numero = numero;
+            Titular = titular;
+            
+        }
     }
 }
--- Sistema_Bancario/Program.cs
+++ Sistema_Bancario/Program.cs
@@ -1,2 +1,16 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Sistema_Bancario;
+using System.ComponentModel;
+
+Conta conta;
+
+Console.WriteLine("Digite o número da conta: ");
+int numero = int.Parse(Console.ReadLine());
+Console.WriteLine("Digite o nome do titular: ");
+string titular = Console.ReadLine();
+Console.WriteLine("Digite se haverá depósito inicial [s/n]: ");
+char resp = char.Parse(Console.ReadLine());
+
+if (resp == 's' || resp == 'S')
+{
+    conta = new Conta()
+}
